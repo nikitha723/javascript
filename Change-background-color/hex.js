c9de5cafccffa167abc5d54110e8d508325fc241
nikitha <nikitha.devan.23@gmail.com>
2020-10-15 16:31:20 -0500

    Added change background color

---
+++ Change-background-color/hex.js
@@ -0,0 +1,21 @@
+const colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function() {
+    
+    let hex = '#'; 
+    for( let i = 0; i < 6; i++){
+        hex += colors[getRandomNumber()];
+    }
+
+    
+    document.body.style.backgroundColor = hex;
+    color.textContent = hex;
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+
+}
